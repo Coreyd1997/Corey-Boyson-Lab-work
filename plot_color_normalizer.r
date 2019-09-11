b9ee19b34a1d3d36a75290e55f2819fb1fd72529
Corey <coreyd1997@gmail.com>
2019-09-11 16:31:52 -0400

    just some minor comment placement modifications

--- plot_color_normalizer.r
+++ plot_color_normalizer.r
@@ -1,12 +1,17 @@
   # This script will be used to standerize the colors within for graphs generated for Somen.
+#importing required libaries 
 library("ggplot2")
 library("readr")
-#importing required libaries 
-#importing of coloe scheme file and data file
+
+
+
 #POC if i can make a vector be called
 #colork<-c("#000000", "#00FF00", "#800080")
-#real import
+#importing of coloe scheme file
 colork<-read_csv("testcolor.csv", col_names = FALSE)$X1
+
+
+
 # # creation of the bargraph with code from ggplot2 bargraph howto? to make sure this works 
 # df <- data.frame(dose=c("D0.5", "D1", "D2"),
 #                  len=c(4.2, 10, 29.5))
@@ -16,4 +21,5 @@ colork<-read_csv("testcolor.csv", col_names = FALSE)$X1
 # p<-ggplot(df, aes(x=dose, y=len, fill=dose)) +
 #   geom_bar(stat="identity")+theme_minimal()
 # p+scale_fill_manual(values=colork)
+# #above is our color csv being used to modifiy fill colors
 #           
